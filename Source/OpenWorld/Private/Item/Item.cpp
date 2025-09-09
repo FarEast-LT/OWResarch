a35cc7d6bd3b777406ac3e7cf6cd3caf5ebe7d78
FarEastLT <theatarisy@gmail.com>
2025-09-09 18:13:20 -0400

    DebugMacros and Item Class setup

--- Source/OpenWorld/Private/Item/Item.cpp
+++ Source/OpenWorld/Private/Item/Item.cpp
@@ -1,27 +1,35 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Item/Item.h"
+#include "engine/engine.h"
+#include "OpenWorld/DebugMacros.h"
 
-// Sets default values
 AItem::AItem()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-// Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	UWorld* World = GetWorld();
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+
+	DRAW_SPHERE_DEBUG(Location);
+	DRAW_LINEANDPOINT_DEBUG(Location, Location + Forward * 300);
 }
 
-// Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	UE_LOG(LogTemp, Warning, TEXT("Hello World"));
 
+	if (GEngine)
+	{
+		FString Message = FString::Printf(TEXT("DeltaTime: %f"), DeltaTime);
+		GEngine->AddOnScreenDebugMessage(0, 60.f, FColor::Blue, Message);
+	}
 }
 
