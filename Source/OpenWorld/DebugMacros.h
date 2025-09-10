a86a37ea9d0c50c9bf8151a2ee851891424f0107
FarEastLT <theatarisy@gmail.com>
2025-09-10 10:19:17 -0400

    WorldOffset

--- Source/OpenWorld/DebugMacros.h
+++ Source/OpenWorld/DebugMacros.h
@@ -2,11 +2,22 @@
 #include "DrawDebugHelpers.h"
 
 #define DRAW_SPHERE_DEBUG(Location) if(GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 24, FColor::Cyan, true, -1.f);
-#define DRAW_LINE_DEBUG(LocationStart, LocationEnd) if(GetWorld()) DrawDebugLine(GetWorld(), LocationStart, LocationEnd, FColor::Cyan, true, -1.f);
+#define DRAW_SPHERE_DEBUG_SingleFrame(Location) if(GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 24, FColor::Cyan, false, -1.f);
+
+#define DRAW_LINE_DEBUG(LocationStart, LocationEnd) if(GetWorld()) DrawDebugLine(GetWorld(), LocationStart, LocationEnd, FColor::Cyan, true, -1.f, 0, 1.f);
+#define DRAW_LINE_DEBUG_SingleFrame(LocationStart, LocationEnd) if(GetWorld()) DrawDebugLine(GetWorld(), LocationStart, LocationEnd, FColor::Cyan, false, -1.f, 0, 1.f);
+
 #define DRAW_POINT_DEBUG(Location) if(GetWorld()) DrawDebugPoint(GetWorld(), Location, 15, FColor::Cyan, true, -1.f);
+#define DRAW_POINT_DEBUG_SingleFrame(Location) if(GetWorld()) DrawDebugPoint(GetWorld(), Location, 15, FColor::Cyan, false, -1.f);
 
 #define DRAW_LINEANDPOINT_DEBUG(LocationStart, LocationEnd) if (GetWorld())\
 {\
 	DrawDebugLine(GetWorld(), LocationStart, LocationEnd, FColor::Cyan, true, -1.f);\
 	DrawDebugPoint(GetWorld(), LocationEnd, 15, FColor::Cyan, true, -1.f);\
+};
+
+#define DRAW_LINEANDPOINT_DEBUG_SingleFrame(LocationStart, LocationEnd) if (GetWorld())\
+{\
+	DrawDebugLine(GetWorld(), LocationStart, LocationEnd, FColor::Cyan, false, -1.f, 0, 1.f);\
+	DrawDebugPoint(GetWorld(), LocationEnd, 15, FColor::Cyan, false, -1.f);\
 };
--- Source/OpenWorld/Private/Item/Item.cpp
+++ Source/OpenWorld/Private/Item/Item.cpp
@@ -11,25 +11,21 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
+	
 
-	UWorld* World = GetWorld();
-	FVector Location = GetActorLocation();
-	FVector Forward = GetActorForwardVector();
-
-	DRAW_SPHERE_DEBUG(Location);
-	DRAW_LINEANDPOINT_DEBUG(Location, Location + Forward * 300);
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
-	UE_LOG(LogTemp, Warning, TEXT("Hello World"));
+	float MovementRate = 50.f; // Speed(cm/s)
+	float RotationRate = 45.f;
 
-	if (GEngine)
-	{
-		FString Message = FString::Printf(TEXT("DeltaTime: %f"), DeltaTime);
-		GEngine->AddOnScreenDebugMessage(0, 60.f, FColor::Blue, Message);
-	}
+	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.0f, 0.0f)); //Speed(cm/s) * Deltaitme(s/f) = cm/f
+	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f)); //Rotation(deg/s) * Deltaitme(s/f) = deg/f
+	
+	DRAW_SPHERE_DEBUG_SingleFrame(GetActorLocation());
+	DRAW_LINEANDPOINT_DEBUG_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 300);
 }
 
