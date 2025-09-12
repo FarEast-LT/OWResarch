58ccdf866b545ce16b2e845108c0bed027d7901f
FarEastLT <theatarisy@gmail.com>
2025-09-11 23:23:53 -0400

    Capsule and SkeletalMesh Component

---
+++ Source/OpenWorld/Private/Pawns/Bird.cpp
@@ -0,0 +1,35 @@
+#include "Pawns/Bird.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+ABird::ABird()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("BirdCapsule"));
+	Capsule->InitCapsuleSize(15.f, 20.f);
+	SetRootComponent(Capsule);
+
+	BirdSkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdSkeletalMesh"));
+	BirdSkeletalMesh->SetupAttachment(GetRootComponent());
+}
+
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
